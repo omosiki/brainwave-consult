60421649ad7fa91976ab64473c8035e50c553cc7
omosiki <abdullateefsikirat6@gmail.com>
2026-03-28 22:13:44 +0100

    Using firebase for the registration form

---
+++ register.js
@@ -0,0 +1,13 @@
+
+import { getStorage, ref as storageRef, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-storage.js";
+  const firebaseConfig = {
+    apiKey: "AIzaSyBvQ4vwG5BRFYzfZVBIGA0UoUOtZe2Or2Y",
+    authDomain: "ijmb-portal-5b7eb.firebaseapp.com",
+    databaseURL: "https://ijmb-portal-5b7eb-default-rtdb.firebaseio.com",
+    projectId: "ijmb-portal-5b7eb",
+    storageBucket: "ijmb-portal-5b7eb.firebasestorage.app",
+    messagingSenderId: "843435985826",
+    appId: "1:843435985826:web:10110e0ed92b95ab2880fb",
+    measurementId: "G-Z4RS7FFXG2"
+  };
+
